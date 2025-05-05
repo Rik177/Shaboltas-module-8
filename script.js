59ad9e7ef0f4a585ae8de2a7caa5cff594d5da30
Rik177 <mefodi12@gmail.com>
2025-05-05 20:51:58 +0300

    loader done

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const galleryButton = document.querySelector(".main__button")
 
 async function getPhotos() {
+    const loader = document.querySelector(".gallery__loader")
+    loader.style.display = "block";
     try {
         const photos = await fetch("https://api.thecatapi.com/v1/images/search?limit=10");
-        if (!photos.ok) { 
+        if (!photos.ok) {
             throw new Error("Что-то пошло не так!")
         }
         let data = await photos.json();
-        if (data) { 
+        if (data) {
             addPhotos(data);
         }
-    } catch(error) { 
+    } catch (error) {
         console.error(error.message);
+    } finally { 
+        loader.style.display = "none";
     }
 }
 function addPhotos(photosArray) {
@@ -19,8 +23,8 @@ function addPhotos(photosArray) {
     photosArray.forEach(obj => {
         const imageElement = document.createElement("img");
         imageElement.src = obj.url;
-        imageElement.style.width = 400 + "px";
-        imageElement.style.height = 300 + "px";
+        // imageElement.style.width = 400 + "px";
+        // imageElement.style.height = 300 + "px";
         imageElement.style.objectFit = "cover";
         imageElement.style.borderRadius = 10 + "px";
         gallery.appendChild(imageElement);
